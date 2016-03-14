beac045c4ae665f279227f711a3ceed63fca4c78
joshkillinger <josh.killinger@gmail.com>
2016-03-14 12:26:10 -0600

    now have rudimentary working trigger-based navigation

---
+++ Assets/Scripts/NavPoint.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavPoint : MonoBehaviour
+{
+    public bool DebugMessages = false;
+
+    PlayerNavigation player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerNavigation>();
+    }
+
+    public void Triggered()
+    {
+        if (DebugMessages)
+        {
+            Debug.Log(gameObject.transform.parent.name + " got triggered event");
+        }
+        player.SetDestination(gameObject.transform.parent.transform.position);
+    }
+
+    public void Entered(bool entered)
+    {
+        if (DebugMessages)
+        {
+            Debug.Log(gameObject.transform.parent.name + " got " + (entered ? "Entered" : "Exited") + " event");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerNavigation.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNavigation : MonoBehaviour
+{
+    public float MoveAccelleration = 2;
+    public float MoveSpeed = 2;
+
+    public bool DebugMessages = false;
+
+    private float currentSpeed;
+
+    private Vector3 destination;
+    private float startMoveTime;
+    private bool moving = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        destination = new Vector3();
+        startMoveTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            Move();
+        }
+    }
+
+    public void SetDestination(Vector3 dest)
+    {
+        destination = dest;
+        moving = true;
+    }
+
+    private void Move()
+    {
+        float moveDist = MoveSpeed * Time.deltaTime;
+        Vector3 direction = destination - gameObject.transform.position;
+
+        if ((moveDist * moveDist) > (Vector3.SqrMagnitude(direction)))
+        {
+            gameObject.transform.position = destination;
+            moving = false;
+        }
+        else
+        {
+            direction.Normalize();
+            gameObject.transform.Translate(direction * moveDist);
+        }
+    }
+}
