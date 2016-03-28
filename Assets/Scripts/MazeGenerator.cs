64e130ac5a545c43fb1828c98256560f33283db6
joshkillinger <josh.killinger@gmail.com>
2016-03-28 12:56:06 -0600

    added lighmaps for test scene, maze generation

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public int MaxRooms = 10;
+    public float BranchOdds = .2f;
+
+    public GameObject RoomPrefab;
+    public GameObject LinePrefab;
+
+    public List<Room> maze;// = new List<Room>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Room start = new Room(0, 0);
+        start.Entrance = true;
+        maze.Add(start);
+
+        CreateRoom(start);
+
+        Debug.Log("Number of rooms generated: " + maze.Count);
+
+        Spawn();
+    }
+
+    /// <summary>
+    /// Creates a new room or connection to an existing room
+    /// </summary>
+    /// <param name="parent">Room to spawn from</param>
+    void CreateRoom(Room parent)
+    {
+        if (maze.Count == MaxRooms)
+        {
+            Debug.Log("Max rooms generated, returning");
+            return;
+        }
+
+        Room.Direction[] directions = parent.AvailableDirections();
+        if (directions.Length == 0)
+        {
+            return;
+        }
+
+        int direction = Random.Range(0, directions.Length);
+
+        Room r = RoomExists(parent.X, parent.Y, directions[direction]);
+        if (r != null)
+        {
+            Debug.Log("Adding connection");
+            parent.Add(r, directions[direction]);
+            r.Add(parent, Room.ReverseDirection(directions[direction]));
+        }
+        else
+        {
+            Debug.Log("Adding Room");
+            AddRoom(parent, directions[direction]);
+        }
+    }
+
+    /// <summary>
+    /// Adds a room to the maze, and potentially spawns more rooms
+    /// </summary>
+    /// <param name="parent">Room that spawned this room</param>
+    /// <param name="direction">Direction from <paramref name="parent"/></param>
+    void AddRoom(Room parent, Room.Direction direction)
+    {
+        Room r = new Room(parent, direction);
+        maze.Add(r);
+
+        int i = 0;
+        while (i < 4)
+        {
+            if (Random.Range(0f, 1f) < BranchOdds)
+                CreateRoom(r);
+            i++;
+        }
+    }
+
+    /// <summary>
+    /// Returns the reference to the room that exists in the maze
+    /// </summary>
+    /// <param name="x">Starting x</param>
+    /// <param name="y">Starting y</param>
+    /// <param name="direction">Offset direction from staring location</param>
+    /// <returns>Reference to existing room, null if does not exist</returns>
+    Room RoomExists(float x, float y, Room.Direction direction)
+    {
+        Room r = null;
+        if (direction == Room.Direction.North)
+            y += 1;
+        else if (direction == Room.Direction.South)
+            y -= 1;
+        else if (direction == Room.Direction.East)
+            x += 1;
+        else if (direction == Room.Direction.West)
+            x -= 1;
+
+        for (int i = 0; i < maze.Count; i++)
+        {
+            if ((maze[i].X == x) && (maze[i].Y == y))
+            {
+                r = maze[i];
+                break;
+            }
+        }
+
+        return r;
+    }
+
+    void Spawn()
+    {
+        foreach (Room r in maze)
+        {
+            
+            Vector3 pos = new Vector3(r.X * 10, 0, r.Y * 10);
+            Debug.Log("Instantiating room at " + pos); 
+            GameObject g = GameObject.Instantiate(RoomPrefab, pos, Quaternion.identity) as GameObject;
+            for (int i = 0; i < 4; i ++)
+            {
+                if (r.Neighbors[i] != null)
+                {
+                    GameObject l = GameObject.Instantiate<GameObject>(LinePrefab);
+                    LineRenderer lr = l.GetComponent<LineRenderer>();
+                    lr.SetPosition(1, Room.DirectionToVector3((Room.Direction)i)* 10);
+                    l.transform.SetParent(g);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Room
+{
+    private Room[] neighbors = { null, null, null, null };
+    public Room[] Neighbors
+    {
+        get { return neighbors; }
+        set { }
+    }
+
+    public bool Entrance = false;
+    public bool Exit = false;
+    public float X = 0f;
+    public float Y = 0f;
+
+    public enum Direction
+    {
+        North = 0,
+        South = 1,
+        East = 2,
+        West = 3
+    }
+
+    public Room(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Room(Room parent, Direction direction)
+    {
+        if (direction == Room.Direction.North)
+        {
+            Y = parent.Y + 1;
+        }
+        else if (direction == Room.Direction.South)
+        {
+            Y = parent.Y - 1;
+        }
+        else if (direction == Room.Direction.East)
+        {
+            X = parent.X + 1;
+        }
+        else
+        {
+            X = parent.X - 1;
+        }
+
+        parent.Add(this, direction);
+        neighbors[(int)ReverseDirection(direction)] = parent;
+    }
+
+    /// <summary>
+    /// Gets an array of available directions for a hallway
+    /// </summary>
+    /// <returns>Directions available for spawning</returns>
+    public Direction[] AvailableDirections()
+    {
+        List<Direction> directions = new List<Direction>();
+
+        if (neighbors[(int)Direction.North] == null)
+        {
+            directions.Add(Direction.North);
+        }
+        if (Y > 0)
+        {
+            if (neighbors[(int)Direction.South] == null)
+            {
+                directions.Add(Direction.South);
+            }
+            
+        }
+        if (neighbors[(int)Direction.East] == null)
+        {
+            directions.Add(Direction.East);
+        }
+        if (X > 0)
+        {
+            if (neighbors[(int)Direction.West] == null)
+            {
+                directions.Add(Direction.West);
+            }
+        }
+
+        return directions.ToArray();
+    }
+
+    /// <summary>
+    /// Add the specified room as a connection to this one
+    /// </summary>
+    /// <param name="other">Room to add</param>
+    /// <param name="direction">Direction to add room</param>
+    public void Add(Room other, Direction direction)
+    {
+        neighbors[(int)direction] = other;
+    }
+
+    /// <summary>
+    /// Reverses a direction
+    /// </summary>
+    public static Direction ReverseDirection(Direction d)
+    {
+        int r = (int)d ^ 0x1;
+        return (Direction)r;
+    }
+
+    /// <summary>
+    /// Converts a direction into a Vector3, with North = (0, 0, 1) and East = (1, 0, 0)
+    /// </summary>
+    public static Vector3 DirectionToVector3(Direction d)
+    {
+        if (d == Direction.North)
+            return Vector3.forward;
+        else if (d == Direction.South)
+            return Vector3.back;
+        else if (d == Direction.East)
+            return Vector3.right;
+        else
+            return Vector3.left;
+    }
+}
