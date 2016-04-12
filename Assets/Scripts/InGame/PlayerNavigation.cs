b8e3b479f15e02be180cbf7a1751ad3d8001b425
joshkillinger <josh.killinger@gmail.com>
2016-04-11 23:36:54 -0600

    WIP fade on maze complete, may need complete overhaul

--- Assets/Scripts/InGame/PlayerNavigation.cs
+++ Assets/Scripts/InGame/PlayerNavigation.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
@@ -6,6 +7,9 @@ public class PlayerNavigation : MonoBehaviour
 {
     public float MoveAccelleration = 2;
     public float MoveSpeed = 2;
+
+    public float FadeTime = 3;
+    public Image[] FadeImages;
 
     public bool DebugMessages = false;
 
@@ -18,13 +22,19 @@ public class PlayerNavigation : MonoBehaviour
     private float startTime;
     private bool timing;
 
+    private bool done = false;
+
     // Use this for initialization
     void Start()
     {
         destination = new Vector3();
         startMoveTime = Time.time;
 
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName("maze"));
+        //clean up the old generation scene
+        Scene gen = SceneManager.GetSceneByName("generation");
+        Scene maze = SceneManager.GetSceneByName("maze");
+        SceneManager.SetActiveScene(maze);
+        SceneManager.MergeScenes(gen, maze);
     }
 
     // Update is called once per frame
@@ -36,10 +46,18 @@ public class PlayerNavigation : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// If the player is not moving, sets a new destination and starts moving
+    /// </summary>
+    /// <param name="dest">World position to move to</param>
     public void SetDestination(Vector3 dest)
     {
+        if (moving || done)
+            return;
+
         destination = dest;
         moving = true;
+
         if (!timing)
         {
             startTime = Time.time;
@@ -47,6 +65,9 @@ public class PlayerNavigation : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Moves the player to the specified destination
+    /// </summary>
     private void Move()
     {
         float moveDist = MoveSpeed * Time.deltaTime;
@@ -64,9 +85,32 @@ public class PlayerNavigation : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Inform the player that the exit has been reached, and ends the level
+    /// </summary>
     public void ReachedExit()
     {
         float duration = Time.time - startTime;
-        Debug.Log("Maze finished, time = " + duration);
+        StartCoroutine(EndLevel(duration));
+    }
+
+    /// <summary>
+    /// Fades the screen to black and ends the level
+    /// </summary>
+    private IEnumerator EndLevel(float completedTime)
+    {
+        float start = Time.time;
+        while ((Time.time - start) < FadeTime)
+        {
+            float alpha = (Time.time - start)/FadeTime;
+            Debug.Log("Alpha is now " + alpha);
+            foreach(Image i in FadeImages)
+            {
+                Color c = i.color;
+                c.a = alpha;
+                i.color = c;
+            }
+            yield return null;
+        }
     }
 }
